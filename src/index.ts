c314d219939592a7dde70d7b4e396d7414ebd66b
Nick Ekstrom <nekstrom98@gmail.com>
2021-03-23 19:57:56 -0500

    pulls data to npm console

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,21 @@
 import express from 'express';
-const app = express();
-const PORT = 8080;
-app.get('/', (req, res) => res.send('Express and Typescript Server'));
-app.listen(PORT, () => {
+import firebase from 'firebase';
+firebase.initializeApp({
+	apiKey: "",
+	authDomain: "accountability-app-29b4b.firebaseapp.com",
+	databaseURL: "https://accountability-app-29b4b-default-rtdb.firebaseio.com",
+	projectId: "accountability-app-29b4b",
+	appId: "",
+});
+var database = firebase.database();
+database.ref('Users/').get().then(function(name){
+	if(name.exists()){
+		console.log(name.val())
+	}
+});
+const server = express();
+const PORT = 8082;
+server.get('/', (req, res) => res.send('Express and Typescript Server'));
+server.listen(PORT, () => {
     console.log(`[server]: Server is running at http://localhost:${PORT}`)
 })
